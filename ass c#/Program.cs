defb98e51e86988865bec75ac4ba5c0aca5c2f2c
ss <ss@SecHex-7QGGYJL>
2025-06-30 19:05:39 +0300

    ass

--- ass c#/Program.cs
+++ ass c#/Program.cs
@@ -1,10 +1,118 @@
-﻿namespace ass_c_
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ass_c_
 {
+    class person { public string name; }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1-Write a program that allows the user to enter a number then print it
+            //Console.WriteLine("enter a number : ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //Console.WriteLine(number);
+            #endregion
+
+            #region Q2- Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen
+
+            //string oo = "1125asda";
+            ////int A = Convert.ToInt16(oo);
+            //int A = int.Parse(oo);
+            //Console.WriteLine(A);
+            //exeption handling oo is not in a corect format
+            #endregion
+            #region Q3-Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            //float x = 11.22f;
+            //float y = 2.0f;
+            //float z = x * y;
+            //Console.WriteLine(z);
+            #endregion
+            #region Q4-Write C# program that Extract a substring from a given string.
+            //string A = "omar salama";
+            //string x = A.Substring(0, 4);
+            //Console.WriteLine(x);
+
+            #endregion
+
+            #region Q5-Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            //int A = 2;
+            //int B = A;
+            // A = 5;
+            //Console.WriteLine($"A = {A} B = {B}");
+            // dont happen any thing
+            #endregion
+            #region Q6-Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+            //person p1 = new person();
+            //p1.name = "omar";
+            //person p2 = p1;
+            //p2.name = "Ahmed";
+            //Console.WriteLine(p1.name);
+            //Console.WriteLine(p2.name);
+            // same name in p1 when modifying p2.name 
+            #endregion
+            #region Q7-Write C# program that take two string variables and print them as one variable
+            //string A = "AAAAAAAAAA";
+            //string B = "BBBBBBBBBB";
+            //Console.WriteLine(A + B);
+            #endregion
+            #region Q8-Write a program that calculates the simple interest given the principal amount, rate of interest, and time. The formula for simple interest is Interest = (principal * rate * time) / 100.
+            //Console.WriteLine("enter a principal : ");
+            //decimal.TryParse(Console.ReadLine(), out decimal principal);
+            //Console.WriteLine("enter a rate  : ");
+            //decimal.TryParse(Console.ReadLine(), out decimal rate);
+            //Console.WriteLine("enter a time   : ");
+            //decimal.TryParse(Console.ReadLine(), out decimal time);
+            //Console.WriteLine($"your interest is = {( principal * rate * time ) /100  }");
+            #endregion
+            #region Q9-Write a program that calculates the Body Mass Index (BMI) given a person's weight in kilograms and height in meters. The formula for BMI isBMI = (Weight) / (Height * Height)
+            //Console.WriteLine("enter a weight in kilograms : ");
+            //double.TryParse(Console.ReadLine(), out double weight);
+            //Console.WriteLine("enter a height in meters  : ");
+            //double.TryParse(Console.ReadLine(), out double height);
+            //Console.WriteLine($"your BMI is = {(weight) / (height * height)}");
+
+            #endregion
+            #region Q10-Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in a variable then display the result. Assume that below 10 degrees is "Just Cold", above 30 degrees is "Just Hot", and anything else is "Just Good"
+            //Console.WriteLine("enter a temperture : ");
+            //int.TryParse(Console.ReadLine(), out int temperture);
+            //string result = (temperture < 10) ? "Just Cold" : ((temperture > 30) ? "Just Hot" : "Just Good" ) ;
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q11-Write a program that takes the date from the user and displays it in various formats using string interpolation
+            //Console.Write("Enter day: ");
+            //int.TryParse(Console.ReadLine(), out int day);
+            //Console.Write("Enter month: ");
+            //int.TryParse(Console.ReadLine(), out int month);
+            //Console.Write("Enter year: ");
+            //int.TryParse(Console.ReadLine(), out int year);
+
+            //Console.WriteLine($"date : {day} , {month} , {year}");
+            //Console.WriteLine($"date : {day} / {month} / {year}");
+            //Console.WriteLine($"date : {day} – {month} – {year}");
+
+            #endregion
+
+            #region What is the output of the following C# code?
+            //The event is on 06/14/2024
+
+            #endregion
+            #region Which of the following statements is correct about the C#.NET code snippet given below?
+            //A value 1 will be assigned to d.
+
+            #endregion
+            #region Which of the following is the correct output for the C# code given below?
+            //6 1
+            #endregion
+
+            #region 15-What will be the output of the C# code given below?
+            // 7 7
+
+
+            #endregion
         }
+
     }
 }
